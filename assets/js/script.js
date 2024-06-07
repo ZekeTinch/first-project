0a632b9b84f8f99e8974d170efde88fc86c69026
Zeke Tinch <zeketinch@gmail.com>
2024-06-06 20:01:28 -0500

    added api

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,22 @@ const settings = {
 	}
 };
 
+// EBay API test
+const settings2 = {
+    async: true,
+    crossDomain: true,
+    url: 'https://ebay32.p.rapidapi.com/search/iphone?page=1&country=germany&country_code=de',
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': '122586d94dmsh85bd15fe85ef9a2p1e537bjsn37d5cb377140',
+        'x-rapidapi-host': 'ebay32.p.rapidapi.com'
+    }
+};
+
+$.ajax(settings2).done(function (response) {
+    console.log(response);
+});
+
 $.ajax(settings).done(function (response) {
 	console.log(response);
 });
